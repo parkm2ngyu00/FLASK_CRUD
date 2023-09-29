abb4d634e1b0d6359ed3af7295eb3a66d6b41f41
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-09-30 00:06:05 +0900

    첫 번째 커밋

---
+++ pybo/views/main_views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, url_for
+from pybo.models import Question
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
